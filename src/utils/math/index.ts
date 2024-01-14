57d5306687c3abfc8cfaee5b405f52a416898b6c
iamafanasev <iamafanasev@gmail.com>
2024-01-14 10:06:31 +0300

    add math module

---
+++ src/utils/math/index.ts
@@ -0,0 +1,15 @@
+export const sum = (a: number, b: number) => {
+    return a + b;
+}
+
+export const sub = (a: number, b: number) => {
+    return a - b;
+}
+
+export const mul = (a: number, b: number) => {
+    return a * b;
+}
+
+export const div = (a: number, b: number) => {
+    return a / b;
+}
